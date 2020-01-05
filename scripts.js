ac033aac8359c5dfe3a80fe477e7ad6021c19003
L3p1989 <phogan110@live.com>
2020-01-04 23:40:14 -0700

    removed repeat code

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-name = 'Patrick';
+name = "Patrick";
 
 var name;
 
@@ -7,49 +7,46 @@ console.log(name);
 setName();
 
 function setName() {
-    var name = 'Patrick Hogan';
-    console.log(name);
-};
+  var name = "Patrick Hogan";
+  console.log(name);
+}
 
-console.log('1st test')
+console.log("1st test");
 
 let avg = findAvg(2, 2);
 
-console.log('2nd test')
+console.log("2nd test");
 
 function findAvg(a, b) {
-    console.log('3rd test');
-    var answer = (a + b) / 2;
-    return answer;
-};//end of hoisting exercise
+  console.log("3rd test");
+  var answer = (a + b) / 2;
+  return answer;
+} //end of hoisting exercise
 
 //beginning of scope exercise
-let fruits = ['Peaches', 'apples', 'blueberries'];
-
-
+let fruits = ["Peaches", "apples", "blueberries"];
 
 function showFruits() {
-    let favFruit = fruits[0];
-    console.log(fruits[0]);
-    showFav();
-    function showFav() {
-        console.log(favFruit)
-    }
-    let leastFav = fruits[1]
+  let favFruit = fruits[0];
+  showFav();
+  function showFav() {
+    console.log(favFruit);
+  }
+  let leastFav = fruits[1];
 
-    console.log(leastFav)
-};
+  console.log(leastFav);
+}
 
 showFruits();
 
 helloName();
 
 function helloName() {
-    console.log('Hello ' + name + '!')
-};
+  console.log("Hello " + name + "!");
+}
 
 let randomFun = function() {
-    alert('randomFun is working')
+  alert("randomFun is working");
 };
 
 randomFun();
